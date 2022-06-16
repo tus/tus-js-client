a4bf400e3bc2a33bdbfa72c2841a9588bca7613d
Marius <marius@transloadit.com>
2022-06-16 15:08:52 +0200

    Add Karma test runner

---
+++ test/karma/browserstack.conf.js
@@ -0,0 +1,40 @@
+// Karma configuration for testing using BrowserStack
+const baseConfig = require('./base.conf')
+
+module.exports = (config) => {
+  baseConfig(config)
+  config.set({
+
+    // global config of your BrowserStack account
+    browserStack: {
+      username : process.env.BROWSERSTACK_USERNAME,
+      accessKey: process.env.BROWSERSTACK_KEY,
+    },
+
+    // define browsers
+    customLaunchers: {
+      bs_firefox_latest: {
+        base           : 'BrowserStack',
+        browser        : 'firefox',
+        browser_version: 'latest',
+        os             : 'Windows',
+        os_version     : '10',
+      },
+      bs_firefox_previous: {
+        base           : 'BrowserStack',
+        browser        : 'firefox',
+        browser_version: 'latest-1',
+        os             : 'Windows',
+        os_version     : '10',
+      },
+    },
+
+    browsers: ['bs_firefox_latest', 'bs_firefox_previous'],
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress', 'BrowserStack'],
+
+  })
+}
---
+++ test/karma/puppeteer.conf.js
@@ -0,0 +1,20 @@
+// Karma configuration for testing using Puppeteer
+const baseConfig = require('./base.conf')
+
+//
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = (config) => {
+  baseConfig(config)
+  config.set({
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress'],
+
+    // start these browsers
+    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: ['ChromeHeadless'],
+  })
+}
