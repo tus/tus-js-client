12effd89497053ee353a10aed16d468fcf12647b
Marius Kleidl <marius@transloadit.com>
2024-04-16 12:14:21 +0200

    Move logger.js to TypeScript

--- lib/logger.ts
+++ lib/logger.ts
@@ -2,11 +2,11 @@
 
 let isEnabled = false
 
-export function enableDebugLog() {
+export function enableDebugLog(): void {
   isEnabled = true
 }
 
-export function log(msg) {
+export function log(msg: string): void {
   if (!isEnabled) return
   console.log(msg)
 }
