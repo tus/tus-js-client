9416ca7df0edb02bad64869f5cbdfef790b3e982
Marius Kleidl <marius@transloadit.com>
2024-09-11 15:09:50 +0200

    Drop `node:` protocol for importing Node.js modules

--- demos/cordova/hooks/before_prepare/copy_tus_files.js
+++ demos/cordova/hooks/before_prepare/copy_tus_files.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-const fs = require('node:fs')
-const path = require('node:path')
+const fs = require('fs')
+const path = require('path')
 
 const rootDir = path.join(__dirname, '..', '..')
 const tusJsPath = path.join(rootDir, '..', '..', 'dist', 'tus.js')
