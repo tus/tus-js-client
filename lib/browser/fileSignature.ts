5d82646c109c370f86c447ee6da5c086f8bb110c
Marius Kleidl <marius@transloadit.com>
2025-01-14 08:51:29 +0100

    Comment for TODO

--- lib/browser/fileSignature.ts
+++ lib/browser/fileSignature.ts
@@ -12,6 +12,7 @@ export default function fingerprint(file: UploadInput, options: UploadOptions) {
   if (file instanceof Blob) {
     return Promise.resolve(
       //@ts-expect-error TODO: We have to check the input type here
+      // This can be fixed by moving the fingerprint function to the FileReader class
       ['tus-br', file.name, file.type, file.size, file.lastModified, options.endpoint].join('-'),
     )
   }
