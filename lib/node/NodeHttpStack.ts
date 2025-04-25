b0b46360bb449d6b4578b39bc32f2adb0d70211b
Marius Kleidl <marius@transloadit.com>
2025-04-25 19:06:00 +0200

    Use `.bytes()` since we only need a view, not an ArrayBuffer

--- lib/node/NodeHttpStack.ts
+++ lib/node/NodeHttpStack.ts
@@ -65,7 +65,7 @@ class Request implements HttpRequest {
 
   async send(body?: FileSliceTypes): Promise<HttpResponse> {
     if (body instanceof Blob) {
-      body = await body.arrayBuffer()
+      body = await body.bytes()
     }
 
     return new Promise((resolve, reject) => {
